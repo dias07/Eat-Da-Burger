a433c2471cc448007df8d5f8ac3074145cce83c8
Diyas Asset <diyasasset@Diyass-MacBook-Pro.local>
2019-09-23 16:14:06 -0500

    changed server

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
